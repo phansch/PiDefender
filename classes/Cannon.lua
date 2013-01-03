7ee84bd2f14a7ab3e8fc243a77c8c8b27b7f5f52
Philipp Hansch <info@phansch.net>
2013-01-03 17:48:02 +0100

    Added image for scout ship

--- classes/Cannon.lua
+++ classes/Cannon.lua
@@ -3,27 +3,27 @@ vector = require ".libraries.hump.vector"
 winWidth = love.graphics.getWidth()
 winHeight = love.graphics.getHeight()
 
-local playerCirc = 0.5 --circumference
-local playerRadius = 30
+local cannonCirc = 0.5 --circumference
+local cannonRadius = 30
 local angle = 0
 
-Player = Class{function(self, circleRadius)
+Cannon = Class{function(self, circleRadius)
     self.position = vector.new(winWidth / 2, winHeight/2)
     self.circleRadius = circleRadius
 end}
-Player.speed = 5
+Cannon.speed = 5
 
-function Player:update(dt)
+function Cannon:update(dt)
     self.direction = vector.new(love.mouse.getX(), love.mouse.getY())
 end
 
-function Player:draw()
-    -- Only update player direction when mouse is outside of circle
+function Cannon:draw()
+    -- Only update cannon direction when mouse is outside of circle
     if(self.direction:dist(self.position) > self.circleRadius) then
-       angle = math.atan2(self.position.y-self.direction.y, self.position.x-self.direction.x) + math.pi - playerCirc/2
+       angle = math.atan2(self.position.y-self.direction.y, self.position.x-self.direction.x) + math.pi - cannonCirc/2
     end
 
     love.graphics.print("angle: " .. angle, 30, 10)
     love.graphics.print("Distance center <-> mouse: "..self.position:dist(self.direction), 30, 30)
-    love.graphics.arc( "line", self.position.x, self.position.y, self.circleRadius+playerRadius, angle, angle + playerCirc)
+    love.graphics.arc( "line", self.position.x, self.position.y, self.circleRadius+cannonRadius, angle, angle + cannonCirc)
 end
--- states/game.lua
+++ states/game.lua
@@ -2,18 +2,20 @@ Gamestate.game = Gamestate.new()
 local state = Gamestate.game
 
 Timer = require 'libraries.hump.timer'
-require ".classes.Player"
+require ".classes.Cannon"
 require ".classes.EnemyTriangle"
 
-player = Player(150)
+cannon = Cannon(150)
 triangles = {} --basic enemies
 
 function state:init()
     Timer.addPeriodic(1, function() self.createFighter() end, 5)
+
+    playerImg = love.graphics.newImage("graphics/hexagon.png")
 end
 
 function state:update(dt)
-    player:update(dt)
+    cannon:update(dt)
 
     for i,tri in ipairs(triangles) do
         if not tri:hasCollided(150) then
@@ -25,11 +27,13 @@ function state:update(dt)
 end
 
 function state:draw()
-    player:draw()
+    cannon:draw()
     love.graphics.circle("line", winWidth/2, winHeight/2, 150, 360)
     for i,tri in ipairs(triangles) do
         tri:draw()
     end
+
+    love.graphics.draw(playerImg, love.mouse.getX()-16, love.mouse.getY()-16, 0)
 end
 
 function state:createFighter()
