b84467fedc011520b12e9f8c9b20bcc3e347a487
Philipp Hansch <desk@phansch.net>
2013-01-25 22:45:25 +0100

    Updated menu

--- states/menu.lua
+++ states/menu.lua
@@ -1,24 +1,13 @@
 Gamestate.menu = Gamestate.new()
 local state = Gamestate.menu
 
-state.text = "Thanks for trying out the game.\n\n"
-            .."Instructions: Your goal is to hold out as long as possible by "
-            .."killing and avoiding enemies. Use [space] to shoot.\n\n\n"
-            .."Now press [space] to continue."
-
 function state:init()
     Introimg = love.graphics.newImage("graphics/intro.png")
     IntroimgSize = vector.new(Introimg:getWidth(), Introimg:getHeight())
 end
 
 function state:draw()
-    --love.graphics.printf(self.text, winWidth/2-200, winHeight/2-200, 400, "center")
-
     love.graphics.draw(Introimg, winWidth/2-IntroimgSize.x/2, winHeight/2-IntroimgSize.y/2, 0)
-
-    love.graphics.setNewFont(11)
-    love.graphics.print("Work in progress. Feel free to submit bugs at http://github.com/phansch/PiDefender.", 15, winHeight-20)
-    love.graphics.setNewFont(12)
 end
 
 function state:keyreleased(key, code)
